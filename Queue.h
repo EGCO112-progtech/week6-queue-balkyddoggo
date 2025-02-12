3d095dca087c2bf513de0d175b300d5c8292808a
balkyddoggo <thitaree.pui@student.mahidol.edu>
2025-02-12 08:54:36 +0000

    ineedsomerest

--- Queue.h
+++ Queue.h
@@ -7,21 +7,30 @@ typedef struct {
 
 
 void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+  NodePtr new_node=(NodePtr) malloc(sizeof(Node));
+  new_node->data=x;
+  if(new_node){
+    q->headPtr;
+    if(q->size==0) q->headPtr=new_node;
+    else q->tailPtr->nextPtr=new_node;
+    q->tailPtr=new_node;
+    q->size++;
+  }
 }
 
 
 int dequeue_struct(Queue *q){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-   return value;
+  NodePtr t=q->headPtr;
+  if(t){
+    if(q->size>0){
+    int value= t->data;
+    if(q->size==1) q->tailPtr=NULL;
+    q->headPtr=t->nextPtr;
+    q->size--;
+    return value;
    }
-   printf("Empty queue");
+  }
+   printf("Empty queue\n");
    return 0;
 }
 
